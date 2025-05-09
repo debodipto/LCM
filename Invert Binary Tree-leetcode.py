3db3775aaf85b3eded6270e2bec51753f66bbe21
Debodioto Samadder <121181135+debodipto@users.noreply.github.com>
2025-05-09 23:20:47 +0600

    Update Invert Binary Tree-leetcode.py

--- Invert Binary Tree-leetcode.py
+++ Invert Binary Tree-leetcode.py
@@ -1,1 +1,17 @@
 
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if not root or (not root.left and not root.right):
+            return root
+
+        tmp = root.left
+        root.left = root.right
+        root.right = tmp
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
