2b851a7bb1b4cc133028c152f2e8fa0bdbd20b44
Debodioto Samadder <121181135+debodipto@users.noreply.github.com>
2025-05-12 21:09:34 +0600

    Update Power of Two-leetcode.java

--- Power of Two-leetcode.java
+++ Power of Two-leetcode.java
@@ -1,1 +1,11 @@
-
+public class Solution {
+    public boolean isPowerOfTwo(int n) {
+        
+        for( int i = 0; i < 31; i++){
+            int powerOfTwo = 1<<i;
+            if( n == powerOfTwo ) return true;
+        }
+        
+        return false;
+    }
+}
