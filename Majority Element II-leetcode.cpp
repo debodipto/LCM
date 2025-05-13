5de4e3f6303c04f487a3fce8f8c041ac9390edd5
Debodioto Samadder <121181135+debodipto@users.noreply.github.com>
2025-05-13 20:44:48 +0600

    Update Majority Element II-leetcode.cpp

--- Majority Element II-leetcode.cpp
+++ Majority Element II-leetcode.cpp
@@ -1,1 +1,17 @@
-
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        int minCount = (int)(n / 3) + 1;
+        unordered_map<int, int> map;
+        vector<int> res;
+        for (int i = 0; i < n; i++){
+            map[nums[i]]++;
+            if (map[nums[i]] == minCount)
+                res.push_back(nums[i]);
+            if (res.size() == 2)
+                break;
+        }
+        return res;
+    }
+};
