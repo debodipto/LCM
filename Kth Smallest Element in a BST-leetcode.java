f45bf2e64452a3fdb2e9a17045845055fc27ea17
Debodioto Samadder <121181135+debodipto@users.noreply.github.com>
2025-05-15 20:28:12 +0600

    Update Kth Smallest Element in a BST-leetcode.java

--- Kth Smallest Element in a BST-leetcode.java
+++ Kth Smallest Element in a BST-leetcode.java
@@ -1,1 +1,18 @@
+class Solution {
+    int ans,count;
 
+    private boolean findKth(TreeNode root,int k){
+        if(root==null)return false;
+        if(findKth(root.left,k)) return true;
+        this.count++;
+        if(this.count==k){
+            this.ans = root.val;
+            return true;
+        }
+        return findKth(root.right,k);
+    }
+    public int kthSmallest(TreeNode root, int k) {
+        findKth(root,k);
+        return this.ans;
+    }
+}
