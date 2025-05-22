05b19242a2423c6cb6eb649c6e93a7f8cd85a793
Debodioto Samadder <121181135+debodipto@users.noreply.github.com>
2025-05-22 13:26:02 +0600

    Update Gennady and a Card Game-codeforces.cpp

--- Gennady and a Card Game-codeforces.cpp
+++ Gennady and a Card Game-codeforces.cpp
@@ -1,1 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin>>s;
+    int n=5,tag=0;
+    while(n--)
+    {
+        string t;
+        cin>>t;
+        if(s[0]==t[0])
+            tag=1;
+        else if(s[1]==t[1])
+            tag=1;
+    }
+    if(tag==1)
+        cout<<"YES"<<endl;
+    else
+        cout<<"NO"<<endl;
 
+    return 0;
+}
